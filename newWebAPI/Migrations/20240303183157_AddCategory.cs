1a30c0a90937357e4ab19a3fe541db5fcc836a48
henrifcorrea <henri.fcorrea1@gmail.com>
2024-03-03 15:52:15 -0300

    Entity Framework Core (Criando conexão de tabelas no banco)

---
+++ newWebAPI/Migrations/20240303183157_AddCategory.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace newWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CatgoryId",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Catgory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Catgory", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CatgoryId",
+                table: "Products",
+                column: "CatgoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Catgory_CatgoryId",
+                table: "Products",
+                column: "CatgoryId",
+                principalTable: "Catgory",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Catgory_CatgoryId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Catgory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_CatgoryId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "CatgoryId",
+                table: "Products");
+        }
+    }
+}
--- newWebAPI/Program.cs
+++ newWebAPI/Program.cs
@@ -2,6 +2,10 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+/// Tornando a classe de conexão do banco de dados em um serviço do AspNet para ser utilizado no código.
+builder.Services.AddDbContext<ApplicationDbContext>();
+
 var app = builder.Build();
 var configuration = app.Configuration;
 ProductRepository.Init(configuration);
@@ -49,31 +53,60 @@ app.Run();
 /// Criando uma classe para armazenar os dados (Simulando um DB)
 public static class ProductRepository {
     public static List<Product> Products { get; set; } = Products = new List<Product>();
-
     public static void Init(IConfiguration config) {
         var products = config.GetSection("Products").Get<List<Product>>();
         Products = products;
     }
-
     public static void Add(Product product) {
         Products.Add(product);
     }
-
     public static Product GetBy(string code) {
         return Products.FirstOrDefault(p => p.Code == code);
     }
-
     public static void Remove(Product product) {
         Products.Remove(product);
     }
 }
 
-/// Criando uma classe 
-public class Product {
-    public string Code { get; set; }
+/// Criando uma classe com as "tabelas" referente as categorias.
+public class Catgory {
+    public int Id { get; set; }
     public string Name { get; set; }
+
 }
 
+/// Criando uma classe com as "tabelas" referente as Tags.
+public class Tag {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int ProductId { get; set; }
+}
+
+/// Criando uma classe com as "tabelas" referente ao produto.
+public class Product {
+    public int Id { get; set; }
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int CatgoryId { get; set; }
+    public Catgory Catgory { get; set; }
+    public List<Tag> Tags { get; set; }
+}
+
+/// Criando uma classe para configurar nosso banco de dados e posteriormente se tornar uma tabela.
 public class ApplicationDbContext : DbContext {
-    
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Product>()
+            .Property(p => p.Description).HasMaxLength(500).IsRequired(false);
+        builder.Entity<Product>()
+            .Property(p => p.Name).HasMaxLength(120).IsRequired();
+        builder.Entity<Product>()
+            .Property(p => p.Code).HasMaxLength(20).IsRequired();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=localhost;Database=Products;User Id=sa;Password=@sql2019;MultipleActiveResultSets=true;Encrypt=YES;TrustServerCertificate=YES");
 }
